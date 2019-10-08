deac348c75f00e8c4c032c11310960baa04df72d
Akshat Giri <akshatgiri@fb.com>
2019-10-07 23:00:42 -0700

    Adding loading indicator on select doctor screen

--- lib/src/select_doctor.dart
+++ lib/src/select_doctor.dart
@@ -15,14 +15,19 @@ class SelectDoctor extends StatefulWidget {
 // screen 1
 class _SelectDoctorState extends State<SelectDoctor> {
   List<Doctor> doctors = [];
+  bool _loading = false;
   @override
   initState() {
     super.initState();
+    setState(() {
+      this._loading = true;
+    });
     // fetching doctors and setting state
     // TODO: Add a loader while loading data
     Doctor.fetchAllDoctors().then((docs) {
       setState(() {
         this.doctors = docs;
+        this._loading = false;
       });
     });
   }
@@ -35,38 +40,49 @@ class _SelectDoctorState extends State<SelectDoctor> {
         ),
         body: ListView(
           padding: const EdgeInsets.all(16),
-          children: doctors
-              .map((doctor) => InkWell(
-                    child: Container(
-                        decoration: BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(
-                                  color: Theme.of(context).dividerColor)),
-                        ),
-                        padding: EdgeInsets.all(16.0),
-                        child: Row(
-                          children: <Widget>[
-                            Text(doctor.name),
-                            Spacer(),
-                            Icon(
-                              Icons.chevron_right,
-                              color: Colors.grey,
-                              size: 24.0,
+          children: this._loading
+              ? [
+                  Center(
+                    child: SizedBox(
+                      child: CircularProgressIndicator(),
+                      height: 24.0,
+                      width: 24.0,
+                    ),
+                  )
+                ]
+              : doctors
+                  .map((doctor) => InkWell(
+                        child: Container(
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                    color: Theme.of(context).dividerColor),
+                              ),
                             ),
-                          ],
-                        )),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => AppointmentForm(
-                            selectedDoctor: doctor,
-                          ),
-                        ),
-                      );
-                    },
-                  ))
-              .toList(),
+                            padding: EdgeInsets.all(16.0),
+                            child: Row(
+                              children: <Widget>[
+                                Text(doctor.name),
+                                Spacer(),
+                                Icon(
+                                  Icons.chevron_right,
+                                  color: Colors.grey,
+                                  size: 24.0,
+                                ),
+                              ],
+                            )),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => AppointmentForm(
+                                selectedDoctor: doctor,
+                              ),
+                            ),
+                          );
+                        },
+                      ))
+                  .toList(),
         )
 
         // Column(
