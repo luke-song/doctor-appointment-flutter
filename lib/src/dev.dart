3d114450e28b18c610ea0f98be728e938ddbd64f
Akshat Giri <akshatgiri@fb.com>
2019-10-07 22:49:26 -0700

    UI changes for select doctor screen

---
+++ lib/src/dev.dart
@@ -0,0 +1,1 @@
+final bool isEnglish = true;
--- lib/src/select_doctor.dart
+++ lib/src/select_doctor.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import '../models/doctor.dart';
 import './appointment_form.dart';
+import './dev.dart';
+
+final String appBarText = isEnglish ? 'Select a doctor' : '송현민';
 
 class SelectDoctor extends StatefulWidget {
   @override
@@ -27,26 +30,48 @@ class _SelectDoctorState extends State<SelectDoctor> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('예약하고 싶으신 의사를 선택해주세요'),
-      ),
-      body: Column(
-        children: doctors
-            .map((doctor) => RaisedButton(
-          child: Text(doctor.name),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => AppointmentForm(
-                  selectedDoctor: doctor,
-                ),
-              ),
-            );
-          },
-        ))
-            .toList(),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(appBarText),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(16),
+          children: doctors
+              .map((doctor) => InkWell(
+                    child: Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                                  color: Theme.of(context).dividerColor)),
+                        ),
+                        padding: EdgeInsets.all(16.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text(doctor.name),
+                            Spacer(),
+                            Icon(
+                              Icons.chevron_right,
+                              color: Colors.grey,
+                              size: 24.0,
+                            ),
+                          ],
+                        )),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AppointmentForm(
+                            selectedDoctor: doctor,
+                          ),
+                        ),
+                      );
+                    },
+                  ))
+              .toList(),
+        )
+
+        // Column(
+        // children:
+        // ),
+        );
   }
 }
